512167933a81c3a0f0980fadfe4294f0d81b6ad9
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-18 20:51:15 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array= array.slice(0);
+  array= array.slice(1);
    return array;
   
 }
